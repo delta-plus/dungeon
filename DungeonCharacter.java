fbfd1bbc4ad43c0f3b52db4264e305838b5f89c5
delta-plus <j03grassl@gmail.com>
2020-03-06 10:30:17 -0800

    More timing

--- DungeonCharacter.java
+++ DungeonCharacter.java
@@ -80,7 +80,7 @@ public abstract class DungeonCharacter
 			controller.updateView(this, "Heal!");
 			TimeUnit.SECONDS.sleep(1);
 			controller.updateView(this, hitPointChange);
-			TimeUnit.SECONDS.sleep(1);
+                        TimeUnit.SECONDS.sleep(1);
 		}
 	}
 
@@ -104,7 +104,7 @@ public abstract class DungeonCharacter
 		else
 		{
 			controller.updateView(opponent, "Miss!");
-			TimeUnit.SECONDS.sleep(1);
+                        TimeUnit.SECONDS.sleep(1);
 		}
 	}
 }
