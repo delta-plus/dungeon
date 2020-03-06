638d6a0de6501d91bd5a9ca37dc8773cfdd32931
Joshua Lansing <jlansing@localhost.localdomain>
2020-03-06 15:33:09 -0800

    Fixed formatting

--- HeroFactory.java
+++ HeroFactory.java
@@ -1,14 +1,23 @@
 package dungeon;
 
-public class HeroFactory {
-    public Hero makeHero(int choice, ViewController controller) {
-        if (choice == 1) {
+public class HeroFactory 
+{
+    public Hero makeHero(int choice, ViewController controller) 
+    {
+        if (choice == 1) 
+        {
             return new Warrior(controller);
-        } else if (choice == 2) {
+        } 
+        else if (choice == 2) 
+        {
             return new Sorceress(controller);
-        } else if (choice == 3) {
+        } 
+        else if (choice == 3) 
+        {
             return new Thief(controller);
-        } else {
+        } 
+        else 
+        {
             System.out.println("invalid choice, returning Thief");
             return new Thief(controller);
         }
